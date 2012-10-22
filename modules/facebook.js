abac0b194bd149ddd62f79e50629eb38a97e206a
suncty <suncty@gmail.com>
2012-10-23 04:22:07 +0900

    facebook 연동 개발중

--- modules/facebook.js
+++ modules/facebook.js
@@ -1,8 +1,34 @@
 var fb = require('fb');
-var appId = config.facebook.appId;
-var appSecret = config.facebook.appSecret;
+var crypto = require('crypto');
+var redis = require('redis');
 
 var modules = global.modules;
 var config = global.config;
 
+var appId = config.facebook.appId;
+var appSecret = config.facebook.appSecret;
+
+exports.test = function(){
+	return 'testzz';
+}
+
+function uniqId(callback) {
+	redis.store.incr('unique_id_' + config.facebook.uniqIdKey, function(err, uid){
+		uid = config.facebook.uniqIdKey + '_' + uid;
+		callback(uid);
+	});
+}
+
+var uniqId_SHA1 = function(callback){
+	uniqId(function(uid){
+		var shasum = crypto.createHash('sha1');
+		shasum.update(uid);
+		callback(shasum.digest('hex'));
+	});
+}
+
 exports.fb = fb;
+exports.appId = appId;
+exports.appSecret = appSecret;
+exports.uniqId = uniqId;
+exports.uniqId_SHA1 = uniqId_SHA1;
--- modules/site.js
+++ modules/site.js
@@ -1,11 +1,51 @@
 var express = require('express');
+var crypto = require('crypto');
+var querystring = require('querystring');
 var app = express();
 
 var modules = global.modules;
 var config = global.config;
 
+var facebook = modules.facebook;
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.cookieSession({key: 'test', secret: 'test'}));
+app.use(express.compress());
+app.use(app.router);
+
+app.use(function(req, res, next){
+	if(!req.session.state)
+	{
+		facebook.uniqId_SHA1(function(uid){
+			console.log('set uid', uid);
+			req.session.state = uid;
+
+			next();
+		});
+	}
+	else next();
+})
+
 app.get('/', function(req, res){
-	res.send('this page is site');
+	if(!req.session.visitCount) req.session.visitCount = 0;
+	res.send('this page is site <br>visit count ' + (++req.session.visitCount));
+});
+
+app.get('/facebook', function(req, res){
+	res.statusCode = 302;
+
+	res.header('Location', 'https://www.facebook.com/dialog/oauth?' + querystring.stringify({
+		cliend_id: facebook.appId,
+		redirect_uri: 'http://bicy.com/facebook',
+		state: req.session.state
+	}));
+	res.end();
 });
 
 exports.app = app;
+
+exports.ready = function(){
+
+}
