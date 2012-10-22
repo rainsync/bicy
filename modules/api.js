53584b6d0d9a2484f2f6ca9394a66d92bc96a577
suncty <suncty@gmail.com>
2012-10-23 02:51:45 +0900

    오타 수정

--- modules/api.js
+++ modules/api.js
@@ -4,7 +4,7 @@ var app = express();
 var modules = global.modules;
 var config = global.config;
 
-app.get('/', function(req, res)
+app.get('/', function(req, res){
 	res.send('this page is api');
 });
 
