17428039547e1dd89ce862763c3633d00b6fcd46
suncty <suncty@gmail.com>
2012-10-23 21:08:14 +0900

    페이스북 로그인 연동 및 친구 목록등을 받아와서 출력함

--- modules/site.js
+++ modules/site.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var crypto = require('crypto');
 var querystring = require('querystring');
+var request = require('request');
+var fb = require('fb');
 var app = express();
 
 exports.app = app;
@@ -9,7 +11,7 @@ exports.ready = function() {
 	var config = global.config;
 	var facebook = modules.facebook;
 
-	app.configure(function(){
+	app.configure(function() {
 		app.use(express.bodyParser());
 		app.use(express.methodOverride());
 		app.use(express.cookieParser());
@@ -18,7 +20,7 @@ exports.ready = function() {
 		app.use(app.router);
 	});
 
-	app.use(function(req, res, next){
+	app.use(function(req, res, next) {
 		if(!req.session.state)
 		{
 			facebook.uniqId_MD5(function(uid){
@@ -31,15 +33,43 @@ exports.ready = function() {
 		else next();
 	});
 
-	app.get('/', function(req, res){
+	app.get('/', function(req, res) {
 		if(!req.session.visitCount) req.session.visitCount = 0;
-		res.send('this page is site <br>visit count ' + (++req.session.visitCount));
+		res.send('this page is site <br>' + 
+			'visit count ' + (++req.session.visitCount) + '<br>' +
+			'facebook access token ' + req.session.accessToken);
 	});
 
-	app.get('/auth/facebook', function(req, res){
+	app.get('/friends', function(req, res) {
+		console.log(req.session.accessToken);
+
+		fb.setAccessToken(req.session.accessToken);
+		fb.api('/me', {fields: ['id', 'name', 'link', 'updated_time', 'friends', 'picture']}, function(resp) {
+			res.send(resp);
+		});
+	});
+
+	app.get('/auth/facebook', function(req, res) {
 		if(req.query["code"])
 		{
-			res.send(String(req.query["code"]));
+			request({
+				uri: "https://graph.facebook.com/oauth/access_token?" + querystring.stringify({
+					client_id: facebook.appId,
+					redirect_uri: 'http://bicy.com/auth/facebook',
+					client_secret: facebook.appSecret,
+					code: String(req.query["code"])
+				})
+			}, function(err, resp, body) {
+				console.log('response');
+				var parse = querystring.parse(body);
+				console.log('access token', parse.access_token);
+
+				req.session.accessToken = parse.access_token;
+
+				res.statusCode = 302;
+				res.header('Location', '/');
+				res.end();
+			});
 		}
 		else
 		{
@@ -47,7 +77,8 @@ exports.ready = function() {
 			res.header('Location', 'https://www.facebook.com/dialog/oauth?' + querystring.stringify({
 				client_id: facebook.appId,
 				redirect_uri: 'http://bicy.com/auth/facebook',
-				state: req.session.state
+				state: req.session.state,
+				scope: 'email,user_birthday,read_stream'
 			}));
 			res.end();
 		}
